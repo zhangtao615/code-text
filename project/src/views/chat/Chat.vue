3401507f25de8508ad2ca5a67ae1ecfbb2f7f397
zhangtao615 <295564283@qq.com>
2019-02-17 21:16:16 +0800

    project

--- project/src/views/chat/Chat.vue
+++ project/src/views/chat/Chat.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="talk-bar">
       <ul>
-        <li v-for="(item,index) in chatList" :key="index">
+        <li v-for="(item,index) in talkList" :key="index">
           <div class="right">
             <span>{{item.question}}</span>
           </div>
@@ -13,104 +13,109 @@
       </ul>
     </div>
     <div class="talk-form">
-      <input type="text" v-model="msg">
-      <button v-on:click="send">发送</button>
+      <input type="text" v-model="msg" id="msgInput">
+      <button @click="send">发送</button>
     </div>
   </div>
 </template>
 <script>
-import md5 from "js-md5";
+import md5 from 'js-md5';
 export default {
-   data() {
+  data() {
     return {
-      msg: "",
-      chatList: []
-    };
+      msg:'',
+      talkList:[],
+    }
   },
-  created() {
+  created(){
     let obj = {
-      title: "智能聊天",
-      className: "chat"
-    };
-    this.$emit("changeNav", obj);
+      title:"聊天",
+      className:"talk"
+    }
+    this.$emit("changeNav",obj);
   },
   methods: {
-    getTimeStamp() {
-      let dateStr = Date.parse(new Date());
-      dateStr = dateStr.toString().substr(0, 10);
+    getTimeStamp(){
+      let dateStr = Date.parse(new Date())
+      dateStr = dateStr.toString().substr(0,10);
       return dateStr;
     },
-    getNonceStr() {
-      let str = Math.random()
-        .toString(36)
-        .substr(2);
+    getNonceStr(){
+      let str = Math.random().toString(36).substr(2);
       return str;
     },
-    getReqSign(params, appkey) {
+    getReqSign(params,appkey){
       let arr = [];
       let obj = {};
-      let str = "";
+      let str = ""
       arr = Object.keys(params).sort();
-      arr.forEach((item, index, arr) => {
-        obj[item] = params[item];
-      });
-      for (let item in obj) {
-        if (obj[item] != "") {
-          str+=item+"="+encodeURI(obj[item])+"&";
+      arr.forEach((item,index,arr)=>{
+        obj[item]= params[item]
+      })
+      for(let item in obj){
+        if(obj[item]!=""){
+          str+=item+"="+encodeURI(obj[item])+"&"
         }
       }
-      str+="app_key="+appkey;
-      str=md5(str).toUpperCase();
-      return str;
-      console.log(str);
-      
+      str += 'app_key='+appkey;
+      str = md5(str).toUpperCase();
+      return str
     },
-    send() {
-      let proxy = "https://bird.ioliu.cn/v2?url=";
-      let url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat";
+    send(){
+      let API_PROXY = 'https://bird.ioliu.cn/v2?url=';
+      let url = 'https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat';
       let params = {
-        app_id: "2111939678",
-        time_stamp: this.getTimeStamp(),
-        nonce_str: this.getNonceStr(),
-        session: "10009",
-        quesion: this.msg,
-        sign: "",
-      };
-      let appkey = "OhsxH5r2TVotMyID";
+        app_id:"2111939678",
+        time_stamp:this.getTimeStamp(),
+        nonce_str:this.getNonceStr(),
+        session:"10000",
+        question:this.msg,
+        sign:""
+      }
+      let appkey = 'OhsxH5r2TVotMyID';
       params.sign = this.getReqSign(params,appkey);
       axios({
-        url: proxy + url,
-        method: "post",
-        data: { ...params }
-      }).then(res => {
+        url:API_PROXY+url,
+        method: 'post',
+        data: {...params},
+      }).then((res)=>{
         let obj = {
-          question: this.msg,
-          answer: res.data.data.answer
-        };
-        this.chatList.push(obj);
-      });
+          question:this.msg,
+          answer:res.data.data.answer
+        }
+        this.talkList.push(obj)
+      })
+      $('#msgInput').val('')
+	    
     }
+    
   }
-};
+}
 </script>
 <style scoped>
-.talk-form {
-  width: 100%;
-  padding-top: 0.3rem;
-  height: 1rem;
-  background-color: #ddd;
-  text-align: center;
-  position: fixed;
-  bottom: 1rem;
-}
-.talk-bar span {
-  padding: 5px 10px;
-  background-color: rgb(195, 240, 195);
-}
-.talk-bar .left {
-  text-align: left;
-}
-.talk-bar .right {
-  text-align: right;
-}
+.talk-form{
+    width: 100%;
+    height: 1rem;
+    background-color: #ddd;
+    text-align: center;
+    position: fixed;
+    bottom: 1rem;
+  }
+  .talk-form input,button{
+    transform: translateY(50%)
+  }
+  .talk-bar{
+    height:100%-2rem;
+  }
+  .talk-bar span{
+    padding:5px 10px;
+    background-color: rgb(195, 240, 195);
+    border-radius: 5px;
+  }
+  .talk-bar .left{
+    text-align: left
+  }
+  .talk-bar .right{
+    text-align: right
+  }
 </style>
